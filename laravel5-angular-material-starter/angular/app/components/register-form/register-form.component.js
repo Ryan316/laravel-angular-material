dc6bc09c2b0185b010d64c0c17258d21afbef6ca
Ryan Damasco <PrivateRyan@new-yosemite-sofware-2015.local>
2017-02-11 13:44:15 -0800

    first commit

---
+++ laravel5-angular-material-starter/angular/app/components/register-form/register-form.component.js
@@ -0,0 +1,40 @@
+class RegisterFormController {
+	constructor(API) {
+		'ngInject';
+
+		this.API = API;
+		this.totalImages = [];
+		this.images = [];
+		this.start = 0;
+		this.end = 5;
+	}
+
+    $onInit(){
+        this.name = '';
+        this.email = '';
+        this.password = '';
+        this.API.all('').get('mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY').then((response) => {
+            this.totalImages = response.photos;
+            for (var i = this.start; i <= this.end; i++) {
+            	this.images.push(this.totalImages[i])
+            }
+        });
+    }
+
+    loadMore() {
+    	this.start += 5;
+	    this.end += 5;
+	    for(let i = this.start+1; i <= this.end; i++) {
+	    	if (!!this.totalImages[i]) {
+	      		this.images.push(this.totalImages[i]);
+	    	}
+	    }
+ 	};
+}
+
+export const RegisterFormComponent = {
+	templateUrl: './views/app/components/register-form/register-form.component.html',
+	controller: RegisterFormController,
+	controllerAs: 'vm',
+	bindings: {}
+}
